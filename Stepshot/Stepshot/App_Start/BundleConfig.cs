97802200352734f68cd8383d97e4f366dcace21c
Rosga <rozzga@gmail.com>
2015-01-22 17:03:52 +0200

    Start page
    Complete start page

--- Stepshot/Stepshot/App_Start/BundleConfig.cs
+++ Stepshot/Stepshot/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace Stepshot
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
